826663026d0c1489b5e40a4538dda3f6ec4814bc
Commoble <josephbettendorff@yahoo.com>
2020-11-27 16:03:13 -0600

    Spawn Entity Feature works nicely now

--- src/main/java/commoble/workshopsofdoom/features/SpawnEntityFeature.java
+++ src/main/java/commoble/workshopsofdoom/features/SpawnEntityFeature.java
@@ -2,8 +2,8 @@ package commoble.workshopsofdoom.features;
 
 import java.util.Optional;
 import java.util.Random;
+import java.util.UUID;
 
-import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import com.mojang.serialization.Codec;
@@ -13,14 +13,16 @@ import commoble.looot.data.DataCodecs;
 import commoble.workshopsofdoom.features.SpawnEntityFeature.EntityConfig;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ILivingEntityData;
+import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.ISeedReader;
 import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.IFeatureConfig;
-import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class SpawnEntityFeature extends Feature<EntityConfig>
@@ -33,12 +35,32 @@ public class SpawnEntityFeature extends Feature<EntityConfig>
 	@Override
 	public boolean generate(ISeedReader reader, ChunkGenerator generator, Random rand, BlockPos pos, EntityConfig config)
 	{
-		ServerWorld world = reader.getWorld();
-		CompoundNBT nbt = config.getSpawnNBT();
-		Entity entity = config.entityType.spawn(world, nbt, null, null, pos, SpawnReason.CHUNK_GENERATION, false, false);
+		Entity entity = config.entityType.create(reader.getWorld());
 		if (entity != null)
 		{
-			return world.addEntity(entity);
+			entity.setLocationAndAngles(pos.getX() + 0.5D, pos.getY() + 0.5D, pos.getZ() + 0.5D, MathHelper.wrapDegrees(rand.nextFloat() * 360.0F), 0.0F);
+			// this bit comes from EntityType::spawn
+			config.getNBT().ifPresent(nbt -> {
+				CompoundNBT entityNbt = entity.writeWithoutTypeId(new CompoundNBT());
+	            UUID uuid = entity.getUniqueID();
+	            entityNbt.merge(nbt);
+	            entity.setUniqueId(uuid);
+	            entity.read(entityNbt);
+			});
+			if (entity instanceof MobEntity)
+			{
+				MobEntity mob = (MobEntity) entity;
+				// if we don't enable persistance
+				// then the entity will despawn instantly
+				// as structures generate well outside of the instant-despawn range
+				// so there's no point in making transient entities via structure generation
+				mob.enablePersistence();
+				mob.onInitialSpawn(reader, reader.getDifficultyForLocation(pos), SpawnReason.STRUCTURE, (ILivingEntityData)null, entity.serializeNBT());
+	            
+			}
+			// add entity and any riders
+			reader.func_242417_l(entity);
+			return true;
 		}
 
 		return false;
@@ -71,16 +93,16 @@ public class SpawnEntityFeature extends Feature<EntityConfig>
 		}
 
 		// get a CompoundNBT that can be used as the nbt parameter for EntityType::spawn
-		public CompoundNBT getSpawnNBT()
-		{
-			return this.getNBT().map(EntityConfig::getNBTWithData).orElse(null);
-		}
-
-		static CompoundNBT getNBTWithData(@Nonnull CompoundNBT configNBT)
-		{
-			CompoundNBT out = new CompoundNBT();
-			out.put("EntityTag", configNBT.copy());
-			return out;
-		}
+//		public CompoundNBT getSpawnNBT()
+//		{
+//			return this.getNBT().map(EntityConfig::getNBTWithData).orElse(null);
+//		}
+//
+//		static CompoundNBT getNBTWithData(@Nonnull CompoundNBT configNBT)
+//		{
+//			CompoundNBT out = new CompoundNBT();
+//			out.put("EntityTag", configNBT.copy());
+//			return out;
+//		}
 	}
 }
