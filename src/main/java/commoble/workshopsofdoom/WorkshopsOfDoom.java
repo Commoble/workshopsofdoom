7f6955c4a6a6e4b0b8251dabb3ae97671b590598
Commoble <josephbettendorff@yahoo.com>
2021-01-30 12:44:47 -0600

    Excavator spawn eggs dispensed from dispensers now dispense excavators instead of excavator spawn eggs

--- src/main/java/commoble/workshopsofdoom/WorkshopsOfDoom.java
+++ src/main/java/commoble/workshopsofdoom/WorkshopsOfDoom.java
@@ -32,12 +32,19 @@ import commoble.workshopsofdoom.structures.LoadableJigsawStructure;
 import commoble.workshopsofdoom.structures.LoadableJigsawStructure.LoadableJigsawConfig;
 import commoble.workshopsofdoom.structures.PillagerJigsawStructure;
 import commoble.workshopsofdoom.util.ConfigHelper;
+import net.minecraft.block.DispenserBlock;
+import net.minecraft.dispenser.DefaultDispenseItemBehavior;
+import net.minecraft.dispenser.IBlockSource;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.ai.attributes.GlobalEntityTypeAttributes;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.SpawnEggItem;
+import net.minecraft.util.Direction;
 import net.minecraft.util.RegistryKey;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.registry.Registry;
@@ -228,6 +235,25 @@ public class WorkshopsOfDoom
 	{
 		// set entity attributes
 		GlobalEntityTypeAttributes.put(this.excavator.get(), ExcavatorEntity.getAttributes().create());
+
+		// add spawn egg behaviours to dispenser
+		DefaultDispenseItemBehavior spawnEggBehavior = new DefaultDispenseItemBehavior()
+		{
+			/**
+			 * Dispense the specified stack, play the dispense sound and spawn particles.
+			 */
+			@Override
+			public ItemStack dispenseStack(IBlockSource source, ItemStack stack)
+			{
+				Direction direction = source.getBlockState().get(DispenserBlock.FACING);
+				EntityType<?> entitytype = ((SpawnEggItem) stack.getItem()).getType(stack.getTag());
+				entitytype.spawn(source.getWorld(), stack, (PlayerEntity) null, source.getBlockPos().offset(direction), SpawnReason.DISPENSER, direction != Direction.UP, false);
+				stack.shrink(1);
+				return stack;
+			}
+		};
+		
+		DispenserBlock.registerDispenseBehavior(this.excavatorSpawnEgg.get(), spawnEggBehavior);
 		
 		// this needs to be called for each Structure instance
 		// structures use this weird placement info per-world instead of feature placements
